68432e62331b7a64cd5063681694183410d33ad2
Gabriel Melo Correia <gmfc@users.noreply.github.com>
2025-06-11 15:05:03 -0300

    feat(ui): add clipboard support

---
+++ ui/hooks/clipboard.ts
@@ -0,0 +1,28 @@
+import { readText as tauriReadText, writeText as tauriWriteText } from "@tauri-apps/api/clipboard";
+
+export async function copyText(text: string): Promise<void> {
+    try {
+        if ((window as any).__TAURI__) {
+            await tauriWriteText(text);
+        } else if (navigator.clipboard) {
+            await navigator.clipboard.writeText(text);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export async function pasteText(): Promise<string> {
+    try {
+        if ((window as any).__TAURI__) {
+            return (await tauriReadText()) ?? "";
+        }
+        if (navigator.clipboard) {
+            return await navigator.clipboard.readText();
+        }
+    } catch (e) {
+        console.error(e);
+    }
+    return "";
+}
+
