d5941b1908a2af9c1f02a4214125261164ebc4a8
Gabriel Melo Correia <gmfc@users.noreply.github.com>
2025-06-12 18:28:24 -0300

    refactor(ulimit): consolidate return

--- apps/cli/programs/ulimit.ts
+++ apps/cli/programs/ulimit.ts
@@ -4,6 +4,8 @@ export async function main(syscall: SyscallDispatcher, argv: string[]): Promise<
     const STDOUT_FD = 1;
     const encode = (s: string) => new TextEncoder().encode(s);
 
+    let result = 0;
+
     if (argv.length === 0) {
         const res = await syscall('set_quota');
         await syscall(
@@ -11,24 +13,23 @@ export async function main(syscall: SyscallDispatcher, argv: string[]): Promise<
             STDOUT_FD,
             encode('cpu ' + res.quotaMs + ' total ' + res.quotaMs_total + ' mem ' + res.quotaMem + '\n'),
         );
-        return 0;
-    }
-
-    let ms: number | undefined;
-    let mem: number | undefined;
-    let total: number | undefined;
-    for (let i = 0; i < argv.length; i++) {
-        if (argv[i] === '-t') {
-            ms = parseInt(argv[i + 1] || '0', 10);
-            i++;
-        } else if (argv[i] === '-m') {
-            mem = parseInt(argv[i + 1] || '0', 10);
-            i++;
-        } else if (argv[i] === '-T') {
-            total = parseInt(argv[i + 1] || '0', 10);
-            i++;
+    } else {
+        let ms: number | undefined;
+        let mem: number | undefined;
+        let total: number | undefined;
+        for (let i = 0; i < argv.length; i++) {
+            if (argv[i] === '-t') {
+                ms = parseInt(argv[i + 1] || '0', 10);
+                i++;
+            } else if (argv[i] === '-m') {
+                mem = parseInt(argv[i + 1] || '0', 10);
+                i++;
+            } else if (argv[i] === '-T') {
+                total = parseInt(argv[i + 1] || '0', 10);
+                i++;
+            }
         }
+        await syscall('set_quota', ms, mem, total);
     }
-    await syscall('set_quota', ms, mem, total);
-    return 0;
+    return result;
 }
